86a5f9294b302fc5c5c91f780259cded5b2461a9
Syed Rehan <40541331+rehan1124@users.noreply.github.com>
2022-05-22 20:21:33 +0530

    "NEW GAME" button
    Implemented "NEW GAME" button functionality

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
 "use strict";
-
-// Score web elements of 2 players
+/*
+FIRST: Define all web elements
+*/
+// Final score web elements of 2 players
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+
 // Dice element
 const diceEl = document.querySelector(".dice");
+
 // New game button
 const newGameBtn = document.querySelector(".btn--new");
+
 // Role dice button
 const rollDiceBtn = document.querySelector(".btn--roll");
+
 // Hold button
 const holdBtn = document.querySelector(".btn--hold");
+
 // Player 1 & 2 current score
 const player1CurrentScoreEl = document.getElementById("current--0");
 const player2CurrentScoreEl = document.getElementById("current--1");
 
+// Player 1 & 2 sections
+const player1Section = document.querySelector(".player--0");
+const player2Section = document.querySelector(".player--1");
+
+/*
+SECOND: Initialize variables which will hold some values and change
+*/
+// When new game begins
 let currentScore = 0;
+
 // 0 and 1 to set active inactive players
 let activePlayer = 0;
-let finalScores = [0, 0];
-let playing = true;
 
+// Final scores of both the players (Initial stage)
+let finalScores = [0, 0];
+
+// Decides whether game can be played or not
+let playing = true;
+console.log(`Game is played? ${playing}`);
+
+/*
+THIRD: Implement functions (DRY)
+*/
 const hideTheDice = function () {
   diceEl.classList.add("hidden");
 };
@@ -29,6 +53,7 @@ const showTheDice = function () {
   diceEl.classList.remove("hidden");
 };
 
+// Not used anywhere
 const updateScore = function (player, score) {
   player.textContent = score;
 };
@@ -43,15 +68,33 @@ const switchPlayers = function (player) {
     .classList.toggle("player--active");
 };
 
-// Set score to ZERO
+const declareWinner = function () {
+  document.querySelector(".player--active").classList.add("player--winner");
+};
+
+const resetWinner = function () {
+  player1Section.classList.remove("player--winner");
+  player2Section.classList.remove("player--winner");
+};
+
+/*
+-------------------
+--- GAME BEGINS ---
+-------------------
+*/
+// Set Final/Initial score to ZERO
 score0El.textContent = 0;
 score1El.textContent = 0;
 
 // Hide the dice appearing on screen
 hideTheDice();
 
+/*
+USER CLICKS ON "ROLL DICE" BUTTON
+*/
 rollDiceBtn.addEventListener("click", function () {
   if (playing) {
+    console.log(`Game is played? ${playing}`);
     // Generate randowm dice roll
     const dice = Math.trunc(Math.random() * 6 + 1);
 
@@ -65,7 +108,6 @@ rollDiceBtn.addEventListener("click", function () {
       currentScore += dice;
       document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
-      // updateScore(player1CurrentScoreEl, currentScore);
 
       // When dice value is 1, switch players.
     } else {
@@ -74,25 +116,25 @@ rollDiceBtn.addEventListener("click", function () {
       currentScore = 0;
       activePlayer = activePlayer === 0 ? 1 : 0;
       switchPlayers(activePlayer);
-      // document.getElementById(`current--${activePlayer}`).textContent =
-      //   currentScore;
     }
   }
 });
 
+/*
+USER CLICKS ON "HOLD" BUTTON
+*/
 holdBtn.addEventListener("click", function () {
   if (playing) {
-    // Update score based on active player for the game
+    console.log(`Game is played? ${playing}`);
+    // Update final score based on active player for the game
     finalScores[activePlayer] += currentScore;
     updateFinalScore(activePlayer, finalScores[activePlayer]);
 
     if (finalScores[activePlayer] >= 20) {
-      document.getElementById(`name--${activePlayer}`).textContent = `Player ${
-        activePlayer + 1
-      } won the game !!!`;
-      document.querySelector(".player--active").classList.add("player--winner");
+      declareWinner();
       switchPlayers(activePlayer);
       playing = false;
+      console.log(`Game is played? ${playing}`);
       hideTheDice();
     } else {
       switchPlayers(activePlayer);
@@ -103,4 +145,23 @@ holdBtn.addEventListener("click", function () {
       switchPlayers(activePlayer);
     }
   }
+});
+
+/*
+USER CLICKS ON "NEW GAME" BUTTON
+*/
+newGameBtn.addEventListener("click", function () {
+  hideTheDice();
+  player1Section.classList.add("player--active");
+  player2Section.classList.remove("player--active");
+  resetWinner();
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  player1CurrentScoreEl.textContent = 0;
+  player2CurrentScoreEl.textContent = 0;
+  playing = true;
+  console.log(`Game is played? ${playing}`);
+  finalScores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
 });
